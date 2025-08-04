2e40f185f99d0c696f4715592394ef0f32a5f6b2
clair <clair.wang102@gmail.com>
2025-08-04 16:19:35 -0400

    added fonts

--- display.js
+++ display.js
@@ -1,17 +1,21 @@
 /* VARIABLES */
-
+let font;
+let italic_font;
 
 /* PRELOAD LOADS FILES */
 function preload(){
-  
+  font = loadFont('fonts/font.ttf');
+  italic_font = loadFont('fonts/italic_font.ttf');
 }
 
 
 /* SETUP RUNS ONCE */
 function setup() {
-  let cnv = createCanvas(400, 400);
+  // Create a canvas and set its parent to the 'canvas-container' div
+  let cnv = createCanvas(1000, 700);
   cnv.parent('canvas-container');
   background('#a79275ff');
+
 }
 
 
